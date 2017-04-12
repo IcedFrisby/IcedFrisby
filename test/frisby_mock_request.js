3f69af52da0452eda210e75488b8567bb5e2cf19
Paul Melnikow <email@paulmelnikow.com>
2017-04-11 22:08:35 -0400

    Add before callback to support plugins

--- test/frisby_mock_request.js
+++ test/frisby_mock_request.js
@@ -162,6 +162,32 @@ describe('Frisby matchers', function() {
     restoreGlobalSetup();
   });
 
+  it('before callbacks should be invoked in sequence before the request', function() {
+    const sequence = [];
+
+    const mockFn = mockRequest.mock()
+      .get('/test-object')
+      .respond({
+          statusCode: 200,
+          body: fixtures.singleObject
+      })
+      .run();
+    const requestFn = function () {
+      sequence.push('request');
+      return mockFn.apply(this, arguments);
+    };
+
+    frisby.create(this.test.title)
+      .before(() => { sequence.push('before-one'); })
+      .before(() => { sequence.push('before-two'); })
+      .get('http://mock-request/test-object', {mock: requestFn})
+      .after(() => {
+        const expectedSequence = ['before-one', 'before-two', 'request'];
+        expect(sequence).to.deep.equal(expectedSequence);
+      })
+      .toss();
+  });
+
   it('expectJSON should test EQUALITY for a SINGLE object', function() {
       // Mock API
       var mockFn = mockRequest.mock()
