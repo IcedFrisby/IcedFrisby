06e3b21273f2167d60114ca9cb26dbf7c1f549ed
Paul Melnikow <email@paulmelnikow.com>
2019-02-19 21:03:24 -0500

    Rewrite some more

--- test/frisby_output.js
+++ test/frisby_output.js
@@ -12,20 +12,25 @@ const { expect } = chai
 //
 
 describe('console output', function() {
-  const warning =
-    '\u001b[33m\u001b[1mWARNING - content-type is json but body type is not set\u001b[22m\u001b[39m\n'
+  let stdout, unhook
+  beforeEach(function() {
+    stdout = ''
+    unhook = intercept(txt => {
+      stdout += txt
+    })
+  })
+  afterEach(function() {
+    unhook()
+    unhook = undefined
+  })
 
-  it("should warn developers if there is a header with 'json' but the body type is not JSON", function() {
-    // Mock API
-    nock('http://mock-request/')
+  it("should warn developers if there is a header with 'json' but the body type is not JSON", async function() {
+    const scope = nock('http://mock-request/')
       .post('/test-object')
       .once()
       .reply(201)
 
-    let unhook
-    let stdout = ''
-
-    frisby
+    await frisby
       .create(this.test.title)
       .addHeaders({
         'Content-Type': 'application/json',
@@ -36,44 +41,28 @@ describe('console output', function() {
         isSomeObj: true,
       })
       .expectStatus(201)
-      .before(() => {
-        unhook = intercept(txt => {
-          stdout += txt
-        })
-      })
-      .after(() => {
-        unhook()
-        chai.assert.equal(
-          warning,
-          stdout,
-          'expect stdout to have a specific warning'
-        )
-      })
-      .toss()
+      .run()
+
+    expect(stdout).to.equal(
+      '\u001b[33m\u001b[1mWARNING - content-type is json but body type is not set\u001b[22m\u001b[39m\n'
+    )
+    scope.done()
   })
 
-  it('should NOT warn developers that "there is a header with \'json\' but the body type is not JSON" because there is no body provided', function() {
-    // Mock API
-    nock('http://mock-request/')
+  it('should NOT warn developers that "there is a header with \'json\' but the body type is not JSON" because there is no body provided', async function() {
+    const scope = nock('http://mock-request/')
       .post('/test-object')
       .once()
-      .reply(201, function(uri, requestBody) {
-        return requestBody
-      })
+      .reply(201, (uri, requestBody) => requestBody)
 
-    let stdout = ''
-    const unhook = intercept(function(txt) {
-      stdout += txt
-    })
-
-    frisby
+    await frisby
       .create(this.test.title)
       .post('http://mock-request/test-object')
       .expectStatus(201)
-      .toss()
+      .run()
 
-    unhook()
-    chai.assert.equal('', stdout, 'expect stdout to be empty')
+    expect(stdout).to.equal('')
+    scope.done()
   })
 
   describe('inspectOnFailure', function() {
@@ -81,31 +70,19 @@ describe('console output', function() {
   })
 
   describe('Deprecated features', function() {
-    it('should error when someone uses globalConfig', function() {
-      const spy = sinon.spy()
-
-      try {
-        frisby.globalSetup({})
-      } catch (err) {
-        expect(err.message).to.equal('globalSetup() has been removed.')
-        spy()
-      }
-      expect(spy.calledOnce).to.equal(true)
+    it('should error when someone uses globalSetup', function() {
+      expect(() => frisby.globalSetup({})).to.throw(
+        Error,
+        'globalSetup() has been removed.'
+      )
     })
 
     it('should error when someone uses reset', function() {
-      //Was used to reset globalConfig
-      const spy = sinon.spy()
-
-      try {
-        frisby.create(this.test.title).reset()
-      } catch (err) {
-        expect(err.message).to.equal(
-          "reset() has been removed from IcedFrisby v2.0+ - there's no more globalSetup(), use config() instead"
-        )
-        spy()
-      }
-      expect(spy.calledOnce).to.equal(true)
+      // Had been used to reset globalConfig.
+      expect(() => frisby.create(this.test.title).reset()).to.throw(
+        Error,
+        "reset() has been removed from IcedFrisby v2.0+ - there's no more globalSetup(), use config() instead"
+      )
     })
   })
 })
